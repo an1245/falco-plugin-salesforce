8751befbf62307891a9ec2e156650b04e3e77f71
an1245 <127995147+an1245@users.noreply.github.com>
2023-12-04 08:41:09 +1300

    Update source.go

--- pkg/salesforce/source.go
+++ pkg/salesforce/source.go
@@ -24,7 +24,7 @@ import (
 
 	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk"
 	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk/plugins/source"
-	"github.com/an1245/falco-plugin-salesforce/pkg/salesforce/grpcclient"
+	"github.com/an1245/falco-plugin-salesforce/pkg/salesforce/sfdcclient/grpcclient"
 )
 
 
