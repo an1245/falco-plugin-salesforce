605bb8a92922a4447d3234e9bfde203b6d5d2b95
an1245 <127995147+an1245@users.noreply.github.com>
2023-12-04 07:42:39 +1300

    Update salesforce.go

--- pkg/salesforce/salesforce.go
+++ pkg/salesforce/salesforce.go
@@ -24,7 +24,6 @@ import (
 	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk"
 	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk/plugins"
 	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk/plugins/source"
-	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk/symbols/extract"
 )
 
 const (
