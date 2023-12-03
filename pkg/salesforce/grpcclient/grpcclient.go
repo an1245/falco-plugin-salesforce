53467020ca4f74ee1ebac2f4da3ddcf945122c06
an1245 <127995147+an1245@users.noreply.github.com>
2023-12-03 18:53:19 +1300

    Update grpcclient.go

--- pkg/salesforce/grpcclient/grpcclient.go
+++ pkg/salesforce/grpcclient/grpcclient.go
@@ -117,7 +117,7 @@ func (c *PubSubClient) GetSchema(schemaId string) (*proto.SchemaInfo, error) {
 // fetch data from the topic. This method will continuously consume messages unless an error occurs; if an error does occur then this method will
 // return the last successfully consumed ReplayId as well as the error message. If no messages were successfully consumed then this method will return
 // the same ReplayId that it originally received as a parameter
-func (c *PubSubClient) Subscribe(replayPreset proto.ReplayPreset, replayId []byte) ([]byte, error) {
+func (c *PubSubClient) Subscribe(replayPreset proto.ReplayPreset, replayId []byte, oCtx *PluginInstance) ([]byte, error) {
         ctx, cancelFn := context.WithCancel(c.getAuthContext())
         defer cancelFn()
 
@@ -190,6 +190,8 @@ func (c *PubSubClient) Subscribe(replayPreset proto.ReplayPreset, replayId []byt
                                 log.Fatal(err)
                         }
 
+			oCtx.grpcChannel <- LoginEventJSON
+}
 
                         // decrement our counter to keep track of how many events have been requested but not yet processed. If we're below our configured
                         // batch size then proactively request more events to stay ahead of the processor
