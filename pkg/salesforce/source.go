7a95e62f165aaaef39e0940a9b3485f981318296
an1245 <127995147+an1245@users.noreply.github.com>
2023-12-04 12:33:05 +1300

    Update source.go

--- pkg/salesforce/source.go
+++ pkg/salesforce/source.go
@@ -107,7 +107,8 @@ func CreateGRPCClient(p *Plugin, oCtx *PluginInstance) {
 		log.Fatalf("the replayId variable must not be populated when the replayPreset variable is set to EARLIEST or LATEST")
 	}
 
-	if (p.Config.debug == true){
+	log.Printf("debug", p.config.debug)
+	if (p.config.debug == true){
 		log.Printf("Creating gRPC client...")
 	}
 	client, err := grpcclient.NewGRPCClient()
