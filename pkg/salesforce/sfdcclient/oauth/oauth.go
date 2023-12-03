871a707f2f088ae0b931c17aa1b3fc4edc219657
an1245 <127995147+an1245@users.noreply.github.com>
2023-12-04 09:03:12 +1300

    Update oauth.go

--- pkg/salesforce/sfdcclient/oauth/oauth.go
+++ pkg/salesforce/sfdcclient/oauth/oauth.go
@@ -9,9 +9,7 @@ import (
 	"strings"
 
 	"github.com/an1245/falco-plugin-salesforce/pkg/salesforce/sfdcclient/common"
-	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk"
-	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk/plugins"
-	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk/plugins/source"
+
 )
 
 const (
