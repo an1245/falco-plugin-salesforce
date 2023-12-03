5a507ca0a756728c9f152e90278975e79cc8c1bb
an1245 <127995147+an1245@users.noreply.github.com>
2023-12-04 07:30:22 +1300

    Update grpcclient.go

--- pkg/salesforce/grpcclient/grpcclient.go
+++ pkg/salesforce/grpcclient/grpcclient.go
@@ -10,7 +10,7 @@ import (
 
         "github.com/falcosecurity/plugin-sdk-go/pkg/sdk"
 	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk/plugins/source"
-	"github.com/an1245/falco-plugin-salesforce/pkg/salesforce/grpcclient/"
+	"github.com/an1245/falco-plugin-salesforce/pkg/salesforce/grpcclient"
         
         "github.com/linkedin/goavro/v2"
         "google.golang.org/grpc"
