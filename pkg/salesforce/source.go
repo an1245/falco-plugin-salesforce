fa838bc962c623dc449cbfd4b088492d548f63d5
an1245 <127995147+an1245@users.noreply.github.com>
2023-12-04 14:42:50 +1300

    Update source.go

--- pkg/salesforce/source.go
+++ pkg/salesforce/source.go
@@ -166,4 +166,76 @@ func CreateGRPCClient(p *Plugin, oCtx *PluginInstance) {
 	}
 }
 
+func CreateGRPCClientConnection(p *Plugin, oCtx *PluginInstance) (grpcclient.PubSubClient){
+	if common.ReplayPreset == proto.ReplayPreset_CUSTOM && common.ReplayId == nil {
+		log.Fatalf("the replayId variable must be populated when the replayPreset variable is set to CUSTOM")
+	} else if common.ReplayPreset != proto.ReplayPreset_CUSTOM && common.ReplayId != nil {
+		log.Fatalf("the replayId variable must not be populated when the replayPreset variable is set to EARLIEST or LATEST")
+	}
+
+	
+	log.Printf("Creating gRPC client...")
+	
+	client, err := grpcclient.NewGRPCClient()
+	if err != nil {
+		log.Fatalf("could not create gRPC client: %v", err)
+	}
+	defer client.Close()
+
+	log.Printf("connection type: %T", client)
+	log.Printf("Populating auth token...")
+	err = client.Authenticate(p.config.SFDCClientId, p.config.SFDCClientSecret, p.config.SFDCLoginURL)
+	if err != nil {
+		client.Close()
+		log.Fatalf("could not authenticate: %v", err)
+	}
+
+	log.Printf("Populating user info...")
+	err = client.FetchUserInfo(p.config.SFDCLoginURL)
+	if err != nil {
+		client.Close()
+		log.Fatalf("could not fetch user info: %v", err)
+	}
+
+	return client
+}
+
+func subscribeGRPCTopic(p *Plugin, oCtx *PluginInstance, client grpcclient.PubSubClient){
+
+	log.Printf("Making GetTopic request...")
+	topic, err := client.GetTopic()
+	if err != nil {
+		client.Close()
+		log.Fatalf("could not fetch topic: %v", err)
+	}
+
+	if !topic.GetCanSubscribe() {
+		client.Close()
+		log.Fatalf("this user is not allowed to subscribe to the following topic: %s", common.TopicName)
+	}
+
+	curReplayId := common.ReplayId
+	for {
+		log.Printf("Subscribing to topic...")
+
+		// use the user-provided ReplayPreset by default, but if the curReplayId variable has a non-nil value then assume that we want to
+		// consume from a custom offset. The curReplayId will have a non-nil value if the user explicitly set the ReplayId or if a previous
+		// subscription attempt successfully processed at least one event before crashing
+		replayPreset := common.ReplayPreset
+		if curReplayId != nil {
+			replayPreset = proto.ReplayPreset_CUSTOM
+		}
+
+		// In the happy path the Subscribe method should never return, it will just process events indefinitely. In the unhappy path
+		// (i.e., an error occurred) the Subscribe method will return both the most recently processed ReplayId as well as the error message.
+		// The error message will be logged for the user to see and then we will attempt to re-subscribe with the ReplayId on the next iteration
+		// of this for loop
+		curReplayId, err = client.Subscribe(replayPreset, curReplayId, oCtx.grpcChannel)
+		if err != nil {
+			log.Printf("error occurred while subscribing to topic: %v", err)
+		}
+	}
+
+}
+
 
