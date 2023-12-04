6e1dd5ab7ba63a52a01acfa32ae6790bbb4402c8
an1245 <127995147+an1245@users.noreply.github.com>
2023-12-04 15:44:47 +1300

    Update grpcclient.go

--- pkg/salesforce/sfdcclient/grpcclient/grpcclient.go
+++ pkg/salesforce/sfdcclient/grpcclient/grpcclient.go
@@ -117,7 +117,7 @@ func (c *PubSubClient) GetSchema(schemaId string) (*proto.SchemaInfo, error) {
 // fetch data from the topic. This method will continuously consume messages unless an error occurs; if an error does occur then this method will
 // return the last successfully consumed ReplayId as well as the error message. If no messages were successfully consumed then this method will return
 // the same ReplayId that it originally received as a parameter
-func (c *PubSubClient) Subscribe(replayPreset proto.ReplayPreset, replayId []byte, grpcchannel chan []byte, topicName string) ([]byte, error) {
+func (c *PubSubClient) Subscribe(replayPreset proto.ReplayPreset, replayId []byte, grpcchannel chan []byte, topicName string, eventName string) ([]byte, error) {
         ctx, cancelFn := context.WithCancel(c.getAuthContext())
         defer cancelFn()
 
