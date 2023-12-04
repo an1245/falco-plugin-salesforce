84ecda17fb77e03b33be5007b0af208dafa08dcf
an1245 <127995147+an1245@users.noreply.github.com>
2023-12-04 17:18:07 +1300

    Update source.go

--- pkg/salesforce/source.go
+++ pkg/salesforce/source.go
@@ -55,7 +55,7 @@ func (p *Plugin) Open(params string) (source.Instance, error) {
 		return nil, err
 	}
 
-	log.Printf("Debug logging is: %s", p.config.SFDCClientId)
+	log.Printf("Debug logging is: %s", p.config.Debuglevel)
 	
 	oCtx.loginChannel = make(chan []byte, 128)
 	oCtx.logoutChannel = make(chan []byte, 128)
