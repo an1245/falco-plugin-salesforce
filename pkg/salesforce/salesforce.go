db5ef93c8f1a629bd2c51a4865bb5c7657674de9
an1245 <127995147+an1245@users.noreply.github.com>
2023-12-06 08:21:06 +1300

    Update salesforce.go

--- pkg/salesforce/salesforce.go
+++ pkg/salesforce/salesforce.go
@@ -58,7 +58,7 @@ type PluginInstance struct {
 	credentialStuffingChannel    chan []byte
 	permissionSetChannel	chan []byte
 	apiAnomalyChannel	chan []byte
-	pubSubClient	*PubSubClient
+	pubSubClient	*grpcclient.PubSubClient
 }
 
 // Return the plugin info to the framework.
