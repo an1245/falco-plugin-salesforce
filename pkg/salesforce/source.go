e07f4ed7bfe61d09d68b2059187bf5c26485ed8e
an1245 <127995147+an1245@users.noreply.github.com>
2023-12-08 16:46:50 +1300

    Update source.go

--- pkg/salesforce/source.go
+++ pkg/salesforce/source.go
@@ -22,12 +22,15 @@ import (
 	"math"
 	"time"
 	"log"
+	"errors"
 	
 	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk"
 	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk/plugins/source"
 	"github.com/an1245/falco-plugin-salesforce/pkg/salesforce/sfdcclient/common"
 	"github.com/an1245/falco-plugin-salesforce/pkg/salesforce/sfdcclient/proto"
 	"github.com/an1245/falco-plugin-salesforce/pkg/salesforce/sfdcclient/grpcclient"
+	"google.golang.org/grpc/status"
+	"google.golang.org/grpc/codes"
 )
 
 
@@ -72,8 +75,9 @@ func (p *Plugin) Open(params string) (source.Instance, error) {
 	// Launch the GRPC client
 	oCtx.pubSubClient = CreateGRPCClientConnection(p, oCtx)
 	
+	
 	go subscribeGRPCTopic(p, oCtx, oCtx.pubSubClient, common.LoginTopic, common.LoginTopicEventType, oCtx.loginChannel)
-	go subscribeGRPCTopic(p, oCtx, oCtx.pubSubClient, common.LogoutTopic, common.LogoutTopicEventType, oCtx.logoutChannel)
+	go subscribeGRPCTopic(p, oCtx, oCtx.pubSubClient, common.LogoutTopic, common.LogoutTopicEventType, oCtx.logoutChannel)	
 	go subscribeGRPCTopic(p, oCtx, oCtx.pubSubClient, common.LoginAsTopic, common.LoginAsTopicEventType, oCtx.loginAsChannel)
 	go subscribeGRPCTopic(p, oCtx, oCtx.pubSubClient, common.SessionHijackingTopic, common.SessionHijackingEventType, oCtx.sessionHijackingChannel)
 	go subscribeGRPCTopic(p, oCtx, oCtx.pubSubClient, common.CredentialStuffingTopic, common.CredentialStuffingEventType, oCtx.credentialStuffingChannel)
@@ -81,6 +85,11 @@ func (p *Plugin) Open(params string) (source.Instance, error) {
 	go subscribeGRPCTopic(p, oCtx, oCtx.pubSubClient, common.ApiAnomalyEventTopic, common.ApiAnomalyEventType, oCtx.apiAnomalyChannel)
 	
 	return oCtx, nil
+}
+
+func exitWithError(oCtx *PluginInstance, err error) {
+	oCtx.pubSubClient.Close()
+	log.Fatalf("Salesforce Plugin ERROR: Exiting with Error: %v", err)
 }
 
 // Closing the event stream and deinitialize the open plugin instance.
@@ -187,9 +196,13 @@ func (o *PluginInstance) NextBatch(pState sdk.PluginState, evts sdk.EventWriters
 
 func CreateGRPCClientConnection(p *Plugin, oCtx *PluginInstance) (*grpcclient.PubSubClient){
 	if common.ReplayPreset == proto.ReplayPreset_CUSTOM && common.ReplayId == nil {
-		log.Fatalf("the replayId variable must be populated when the replayPreset variable is set to CUSTOM")
+		log.Printf("Salesforce Plugin ERROR: the replayId variable must be populated when the replayPreset variable is set to CUSTOM")
+		err := errors.New("the replayId variable must be populated when the replayPreset variable is set to CUSTOM")
+		exitWithError(oCtx, err)
 	} else if common.ReplayPreset != proto.ReplayPreset_CUSTOM && common.ReplayId != nil {
-		log.Fatalf("the replayId variable must not be populated when the replayPreset variable is set to EARLIEST or LATEST")
+		log.Printf("the replayId variable must not be populated when the replayPreset variable is set to EARLIEST or LATEST")
+		err := errors.New("the replayId variable must not be populated when the replayPreset variable is set to EARLIEST or LATEST")
+		exitWithError(oCtx, err)
 	}
 
 	if (p.config.Debug == true){
@@ -198,17 +211,20 @@ func CreateGRPCClientConnection(p *Plugin, oCtx *PluginInstance) (*grpcclient.Pu
 	
 	client, err := grpcclient.NewGRPCClient(p.config.Debug)
 	if err != nil {
-		log.Fatalf("Salesforce Plugin ERROR: could not create gRPC client: %v", err)
+		log.Printf("Salesforce Plugin ERROR: could not create gRPC client: %v", err)
+		exitWithError(oCtx, err)
 	}
-	//defer client.Close()
+
 	if (p.config.Debug == true){
 		log.Printf("Salesforce Plugin: Populating auth token...")
+		
 	}
 	
 	err = client.Authenticate(p.config.SFDCClientId, p.config.SFDCClientSecret, p.config.SFDCLoginURL)
 	if err != nil {
-		client.Close()
-		log.Fatalf("Salesforce Plugin ERROR: could not authenticate: %v", err)
+		
+		log.Printf("Salesforce Plugin ERROR: could not authenticate - check your Client ID and Secret in falco.yaml - %v", err)
+		exitWithError(oCtx, err)
 	}
 
 	if (p.config.Debug == true){
@@ -216,30 +232,32 @@ func CreateGRPCClientConnection(p *Plugin, oCtx *PluginInstance) (*grpcclient.Pu
 	}
 	err = client.FetchUserInfo(p.config.SFDCLoginURL)
 	if err != nil {
-		client.Close()
-		log.Fatalf("Salesforce Plugin ERROR: could not fetch user info: %v", err)
+		log.Printf("Salesforce Plugin ERROR: could not fetch user info: %v", err)
+		exitWithError(oCtx, err)
 	}
 
 	return client
 }
 
-func subscribeGRPCTopic(p *Plugin, oCtx *PluginInstance, client *grpcclient.PubSubClient, Topic string, eventType string, channel chan []byte){
+func subscribeGRPCTopic(p *Plugin, oCtx *PluginInstance, client *grpcclient.PubSubClient, Topic string, eventType string, channel chan []byte) (error){
 
 	if (p.config.Debug == true){
 		log.Printf("Salesforce Plugin: Making GetTopic request...")
 	}
 	topic, err := client.GetTopic(Topic)
 	if err != nil {
-		client.Close()
-		log.Fatalf("Salesforce Plugin ERROR: could not fetch topic: %v", err)
+		log.Printf("Salesforce Plugin ERROR: could not fetch topic: %v", err)
+		exitWithError(oCtx, err)
 	}
 
 	if !topic.GetCanSubscribe() {
-		client.Close()
-		log.Fatalf("Salesforce Plugin ERROR: this user is not allowed to subscribe to the following topic: %s", Topic)
+		log.Printf("Salesforce Plugin ERROR: this user is not allowed to subscribe to the following topic: %s", Topic)
+		exitWithError(oCtx, err)
 	}
 
 	curReplayId := common.ReplayId
+	backoffcount := 1
+
 	for {
 		
 		if (p.config.Debug == true){
@@ -260,11 +278,63 @@ func subscribeGRPCTopic(p *Plugin, oCtx *PluginInstance, client *grpcclient.PubS
 		// of this for loop
 		curReplayId, err = client.Subscribe(replayPreset, curReplayId, channel, Topic, eventType)
 		if err != nil {
-			log.Printf("Salesforce Plugin ERROR: error occurred while subscribing to topic: %v", err)
+			if (processErrorCodeBackoff(err) == false && backoffcount <= 3) {
+				log.Printf("Salesforce Plugin ERROR: error occurred while subscribing to topic - sleeping for %d min", backoffcount*5)
+				time.Sleep(time.Duration(backoffcount * 5) * time.Minute)
+				backoffcount += 1
+			} else {
+				fmt.Errorf("Salesforce Plugin ERROR: error occurred while subscribing to topic: - still existed after 30 mis - exiting..")
+				exitWithError(oCtx, err)
+			}
 			
 		}
 	}
 
 }
 
+
+func processErrorCodeBackoff(err error) bool {
+	
+	// Processes Error code and decides whether to exit immediately of back off
+	// returns true to exit immediately
+	// returns false to backoff
+
+	switch status.Code(err) {
+	case codes.Canceled:
+		return false
+	case codes.Unknown:
+		return false
+	case codes.InvalidArgument:
+		return false
+	case codes.DeadlineExceeded:
+		return false
+	case codes.NotFound:
+		return false
+	case codes.AlreadyExists:
+		return false
+	case codes.PermissionDenied:
+		return false
+	case codes.ResourceExhausted:
+		return false
+	case codes.FailedPrecondition:
+		return false
+	case codes.Aborted:
+		return false
+	case codes.OutOfRange:
+		return false
+	case codes.Unimplemented:
+		return true
+	case codes.Internal:
+		return false
+	case codes.Unavailable:
+		return false
+	case codes.DataLoss: 
+		return false
+	case codes.Unauthenticated:
+		return true
+	default:
+		return false
+	}
+
+}
 
